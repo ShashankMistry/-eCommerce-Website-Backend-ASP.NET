5bd248b1b2b5554d8ec40e8d87c08fcfeff32cb3
Shashank Mistry <shashankmistry30@gmail.com>
2023-03-27 20:20:34 -0400

    Products and Users Model Created

---
+++ eCommerce-Website-Backend-ASP.NET/eCommerce-Website-Backend-ASP.NET/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace eCommerce_Website_Backend_ASP.NET
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ eCommerce-Website-Backend-ASP.NET/eCommerce-Website-Backend-ASP.NET/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace eCommerce_Website_Backend_ASP.NET
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public string Pass { get; set; }
+        public string Address { get; set; }
+    }
+}
