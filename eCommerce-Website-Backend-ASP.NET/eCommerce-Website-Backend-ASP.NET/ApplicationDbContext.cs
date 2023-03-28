33c07212c6a896ba6eb1dec0fdd3b33ad72756f9
Shashank Mistry <shashankmistry30@gmail.com>
2023-03-27 20:21:31 -0400

    DB Context files created

---
+++ eCommerce-Website-Backend-ASP.NET/eCommerce-Website-Backend-ASP.NET/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce_Website_Backend_ASP.NET
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
