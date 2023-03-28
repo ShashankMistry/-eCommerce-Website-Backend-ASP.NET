cb34507a56f6bf693133674148d079e67c8ad449
Shashank Mistry <shashankmistry30@gmail.com>
2023-03-27 20:22:06 -0400

    Cart and Comment Controller Created

---
+++ eCommerce-Website-Backend-ASP.NET/eCommerce-Website-Backend-ASP.NET/Controllers/CartController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce_Website_Backend_ASP.NET
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CartsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cart>>> GetCarts()
+        {
+            return await _context.Carts.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cart>> GetCart(int id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return cart;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Cart>> CreateCart(Cart cart)
+        {
+            _context.Carts.Add(cart);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCart), new { id = cart.Id }, cart);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCart(int id, Cart cart)
+        {
+            if (id != cart.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cart).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCart(int id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            _context.Carts.Remove(cart);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+
+}
---
+++ eCommerce-Website-Backend-ASP.NET/eCommerce-Website-Backend-ASP.NET/Controllers/CommentController.cs
@@ -0,0 +1,101 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce_Website_Backend_ASP.NET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Comment
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comment>>> GetComments()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+
+        // GET: api/Comment/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comment>> GetComment(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return comment;
+        }
+
+        // PUT: api/Comment/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComment(int id, Comment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(comment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Comment
+        [HttpPost]
+        public async Task<ActionResult<Comment>> PostComment(Comment comment)
+        {
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetComment), new { id = comment.Id }, comment);
+        }
+
+        // DELETE: api/Comment/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteComment(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CommentExists(int id)
+        {
+            return _context.Comments.Any(e => e.Id == id);
+        }
+    }
+}
+
